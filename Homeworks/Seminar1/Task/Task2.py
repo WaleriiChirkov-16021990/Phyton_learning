9282df1f480e64a05c6d9fbdf0f633a75edb7cf2
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-09-25 16:07:13 +0300

    Отредактировал задачу 2, добавил цветной вывод в консоль

--- Homeworks/Seminar1/Task/Task2.py
+++ Homeworks/Seminar1/Task/Task2.py
@@ -5,19 +5,36 @@
 print('Проверим истинность указанного утверждения: ¬(X ⋁ Y ⋁ Z) = ¬ X ⋀ ¬ Y ⋀ ¬ Z')
 
 
+def out_red(text):
+    print('\033[31m {}'.format(text))
+
+
+def out_blue(text):
+    print('\033[34m {}'.format(text))
+
+
+def out_green(text):
+    print('\033[32m {}'.format(text))
+
+
+def out_white(text):
+    print('\033[39m {}'.format(text))
+
+
 def True_or_False():
     res = True
     for X in range(2):
         for Y in range(2):
             for Z in range(2):
                 left_Value = not (X or Y or Z)
-                right_Value = not X and not Y and not Z
-                print(f'X = {X}, Y = {Y}, Z = {Z}')
+                right_Value = not Z and not Y and not Z
                 if left_Value != right_Value:
-                    print(False)
+                    out_red(f'X = {X}, Y = {Y}, Z = {Z}')
+                    out_red(False)
                     res = False
                 else:
-                    print(True)
+                    out_green(f'X = {X}, Y = {Y}, Z = {Z}')
+                    out_blue(True)
     return res
 
 
@@ -26,8 +43,8 @@ result = True_or_False()
 if result:
     print('Утверждение истинно для всех значений предикат!')
 else:
-    print("Утверждение ложно !")
-print('Убедимся в этом на практике!')
+    out_red("Утверждение ложно !")
+out_white('Убедимся в этом на практике!')
 print("Введите значения 1 или 0 для 3-х переменных в разных комбинациях\n")
 while True:
     XYZ = ['X', 'Y', 'Z']
